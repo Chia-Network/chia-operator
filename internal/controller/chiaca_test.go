cef0a2f54775563e6617c3388fbed65143405494
Starttoaster <b.butler@chia.net>
2023-12-16 20:46:29 -0800

    Refine minimal example controller tests

---
+++ internal/controller/chiaca_test.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2023 Chia Network Inc.
+*/
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	apiv1 "github.com/chia-network/chia-operator/api/v1"
+)
+
+var _ = Describe("ChiaCA controller", func() {
+	var (
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("When creating ChiaCA", func() {
+		It("should update its Spec with API defaults", func() {
+			By("By creating a new ChiaCA")
+			ctx := context.Background()
+			testCA := &apiv1.ChiaCA{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "k8s.chia.net/v1",
+					Kind:       "ChiaCA",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-chiaca",
+					Namespace: "default",
+				},
+				Spec: apiv1.ChiaCASpec{
+					Secret: "test-secret",
+				},
+			}
+			expect := &apiv1.ChiaCA{
+				Spec: apiv1.ChiaCASpec{
+					Image:  "ghcr.io/chia-network/chia-operator/ca-gen:latest",
+					Secret: "test-secret",
+				},
+			}
+
+			// Create ChiaCA
+			Expect(k8sClient.Create(ctx, testCA)).Should(Succeed())
+
+			// Look up the created ChiaCA
+			lookupKey := types.NamespacedName{Name: testCA.Name, Namespace: testCA.Namespace}
+			createdChiaCA := &apiv1.ChiaCA{}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, lookupKey, createdChiaCA)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			// Ensure the ChiaCA's spec is equal to the expected spec
+			Expect(createdChiaCA.Spec).Should(Equal(expect.Spec))
+		})
+	})
+})
---
+++ internal/controller/chiafarmer_test.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2023 Chia Network Inc.
+*/
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	apiv1 "github.com/chia-network/chia-operator/api/v1"
+)
+
+var _ = Describe("ChiaFarmer controller", func() {
+	var (
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("When creating ChiaFarmer", func() {
+		It("should update its Spec with API defaults", func() {
+			By("By creating a new ChiaFarmer")
+			ctx := context.Background()
+			testFarmer := &apiv1.ChiaFarmer{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "k8s.chia.net/v1",
+					Kind:       "ChiaFarmer",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-chiafarmer",
+					Namespace: "default",
+				},
+				Spec: apiv1.ChiaFarmerSpec{
+					ChiaConfig: apiv1.ChiaFarmerConfigSpec{
+						CommonChiaConfigSpec: apiv1.CommonChiaConfigSpec{
+							CASecretName: "test-secret",
+						},
+						FullNodePeer: "node.default.svc.cluster.local:58444",
+						SecretKeySpec: apiv1.ChiaKeysSpec{
+							Name: "testkeys",
+							Key:  "key.txt",
+						},
+					},
+				},
+			}
+			expect := &apiv1.ChiaFarmer{
+				Spec: apiv1.ChiaFarmerSpec{
+					ChiaConfig: apiv1.ChiaFarmerConfigSpec{
+						CommonChiaConfigSpec: apiv1.CommonChiaConfigSpec{
+							Image:        "ghcr.io/chia-network/chia:latest",
+							CASecretName: "test-secret",
+						},
+						FullNodePeer: "node.default.svc.cluster.local:58444",
+						SecretKeySpec: apiv1.ChiaKeysSpec{
+							Name: "testkeys",
+							Key:  "key.txt",
+						},
+					},
+					CommonSpec: apiv1.CommonSpec{
+						ServiceType:     "ClusterIP",
+						ImagePullPolicy: "Always",
+						ChiaExporterConfig: apiv1.ChiaExporterConfigSpec{
+							Enabled: true,
+							Image:   "ghcr.io/chia-network/chia-exporter:latest",
+						},
+					},
+				},
+			}
+
+			// Create ChiaFarmer
+			Expect(k8sClient.Create(ctx, testFarmer)).Should(Succeed())
+
+			// Look up the created ChiaFarmer
+			lookupKey := types.NamespacedName{Name: testFarmer.Name, Namespace: testFarmer.Namespace}
+			createdChiaFarmer := &apiv1.ChiaFarmer{}
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, lookupKey, createdChiaFarmer)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			// Ensure the ChiaFarmer's spec equals the expected spec
+			Expect(createdChiaFarmer.Spec).Should(Equal(expect.Spec))
+		})
+	})
+})
