82008eff30f3bd2b57891880ce061158acc81f4f
Starttoaster <b.butler@chia.net>
2024-07-17 20:12:38 -0700

    Change order of functions to make diff cleaner

--- internal/controller/chianode/helpers.go
+++ internal/controller/chianode/helpers.go
@@ -210,14 +210,6 @@ func (r *ChiaNodeReconciler) getChiaEnv(ctx context.Context, node k8schianetv1.C
 	return env
 }
 
-// getFullNodePort determines the correct full node port to use
-func (r *ChiaNodeReconciler) getFullNodePort(ctx context.Context, node k8schianetv1.ChiaNode) int32 {
-	if node.Spec.ChiaConfig.Testnet != nil && *node.Spec.ChiaConfig.Testnet {
-		return consts.TestnetNodePort
-	}
-	return consts.MainnetNodePort
-}
-
 // getOwnerReference gives the common owner reference spec for ChiaNode related objects
 func (r *ChiaNodeReconciler) getOwnerReference(ctx context.Context, node k8schianetv1.ChiaNode) []metav1.OwnerReference {
 	return []metav1.OwnerReference{
@@ -229,4 +221,12 @@ func (r *ChiaNodeReconciler) getOwnerReference(ctx context.Context, node k8schia
 			Controller: &consts.ControllerOwner,
 		},
 	}
+}
+
+// getFullNodePort determines the correct full node port to use
+func (r *ChiaNodeReconciler) getFullNodePort(ctx context.Context, node k8schianetv1.ChiaNode) int32 {
+	if node.Spec.ChiaConfig.Testnet != nil && *node.Spec.ChiaConfig.Testnet {
+		return consts.TestnetNodePort
+	}
+	return consts.MainnetNodePort
 }
