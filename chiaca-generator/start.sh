011ac5f1c91001e0b0c911f8c002449c24c63935
Starttoaster <b.butler@chia.net>
2024-08-08 11:19:36 -0700

    switch to new light-weight tool for generating certificate authority secrets (#130)

--- chiaca-generator/start.sh
+++ chiaca-generator/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-cd ${CHIA_ROOT}/config/ssl/ca
-kubectl create secret generic ${SECRET_NAME} --namespace ${NAMESPACE} --from-file=chia_ca.crt --from-file=chia_ca.key --from-file=private_ca.crt --from-file=private_ca.key
+mkdir -p certs
+cd certs
+chia-tools certs generate -o ./
+kubectl create secret generic ${SECRET_NAME} --namespace ${NAMESPACE} --from-file=ca/chia_ca.crt --from-file=ca/chia_ca.key --from-file=ca/private_ca.crt --from-file=ca/private_ca.key
